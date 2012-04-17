2ec5a33e76b8640b00a1695b3f2eb48327e40646
amergey <a_mergey@yahoo.fr>
2012-04-17 16:11:44 +0200

    RAP/RCP single sourcing

---
+++ widgets/datechooser/org.eclipse.nebula.widget.datechooser.rap/src/org/eclipse/nebula/widgets/datechooser/internal/DateChooserComponentImpl.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.eclipse.nebula.widgets.datechooser.internal;
+
+import java.util.Locale;
+
+import org.eclipse.nebula.widgets.datechooser.DateChooser;
+import org.eclipse.rwt.RWT;
+
+/**
+ * RAP implementation
+ * @author amergey
+ *
+ */
+public class DateChooserComponentImpl extends DateChooserComponent {
+
+	@Override
+	public NLSMessages getNLS(Locale locale) {
+		return (NLSMessages) RWT.NLS.getISO8859_1Encoded( DateChooser.BUNDLE_NAME, NLSMessages.class );
+	}
+
+}
